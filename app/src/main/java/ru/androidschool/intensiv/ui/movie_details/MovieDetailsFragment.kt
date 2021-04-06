43300e7c41f238a0367f7700946c5bef937d17c1
Alex <a.m.chanov@mail.ru>
2021-04-06 14:33:59 +0300

    Movie details screens Ui.

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -5,20 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.movie_details_fragment.*
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Movie
 
-private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM1 = "title"
 private const val ARG_PARAM2 = "param2"
 
 class MovieDetailsFragment : Fragment() {
 
-    private var param1: String? = null
+    private var param1: Movie? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
+            param1 = it.getParcelable<Movie>(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
@@ -30,6 +33,22 @@ class MovieDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.movie_details_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setData()
+    }
+
+    private fun setData(){
+        movie_details_title.text = param1?.title
+        param1?.rating?.let {
+            movie_details_rating_bar.rating = param1?.rating!!
+        }
+
+        Picasso.get()
+            .load(R.drawable.placeholder)
+            .into(movie_details_iv)
     }
 
     companion object {
