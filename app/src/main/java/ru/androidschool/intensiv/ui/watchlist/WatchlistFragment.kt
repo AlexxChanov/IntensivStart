f398d29a4b6627ce15949767f11a1c703b8a49cf
Alex <a.m.chanov@mail.ru>
2021-04-19 16:03:12 +0300

    Fixed remarks.

--- app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
@@ -11,24 +11,10 @@ import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import kotlinx.android.synthetic.main.fragment_watchlist.movies_recycler_view
 import ru.androidschool.intensiv.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class WatchlistFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     val adapter by lazy {
         GroupAdapter<GroupieViewHolder>()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -45,17 +31,5 @@ class WatchlistFragment : Fragment() {
 
         movies_recycler_view.layoutManager = GridLayoutManager(context, 4)
         movies_recycler_view.adapter = adapter.apply { addAll(listOf()) }
-    }
-
-    companion object {
-
-        @JvmStatic
-        fun newInstance() =
-            WatchlistFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
